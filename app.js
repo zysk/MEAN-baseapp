5509468ab780d86a1fe07baac54892ada9848b5a
Arijit <arijit.saha@zysk.tech>
2022-08-23 19:44:26 +0530

    Check

--- app.js
+++ app.js
@@ -73,6 +73,7 @@ app.use('*', express.static(path.join(process.cwd(), `/views/production/angular-
 /* Catch 404 and forward to error handler */
 app.use((req, res, next) => next(createError(404)));
 
+
 /* Error handler */
 app.use((err, req, res, next) => {
   // Set locals, only providing error in development
