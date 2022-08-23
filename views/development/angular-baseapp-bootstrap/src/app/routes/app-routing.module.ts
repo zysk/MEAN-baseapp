3860e4de1e17c2cdcb5e718046d6a32b0fdbe522
Arijit <arijit.saha@zysk.tech>
2022-08-24 01:28:24 +0530

    SonarCloud check test 10

---
+++ views/development/angular-baseapp-bootstrap/src/app/routes/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('../modules/user/user.module').then(module => module.UserModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload' })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
